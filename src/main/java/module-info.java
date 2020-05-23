c0e9ffb76489a542fa0037225b8b0028e140f785
Mads Opheim <mads.opheim@gmail.com>
2020-05-24 00:29:53 +0200

    Added module info

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module randomstrings {
+    requires java.ws.rs;
+    requires javax.inject;
+    requires cdi.api;
+    requires microprofile.config.api;
+    requires microprofile.health.api;
+    requires microprofile.metrics.api;
+    requires microprofile.openapi.api;
+}
