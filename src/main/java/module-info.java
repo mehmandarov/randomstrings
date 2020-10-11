cef86db96810d6b9de0102950b76aca454a8315d
Mads Opheim <mads.opheim@gmail.com>
2020-10-11 18:05:12 +0200

    Example POM for Boost

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module randomstrings {
-    requires java.ws.rs;
-    requires javax.inject;
-    requires cdi.api;
-    requires microprofile.config.api;
-    requires microprofile.health.api;
-    requires microprofile.metrics.api;
-    requires microprofile.openapi.api;
-}
