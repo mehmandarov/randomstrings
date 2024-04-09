d0ff449ba5f8c856b3e241587195d08e4dedecfc
Rustam Mehmandarov <mehmandarov@gmail.com>
2024-04-09 07:02:17 -0700

    Minor refactoring for CRaC

--- src/scripts/crac/checkpoint.sh
+++ src/scripts/crac/checkpoint.sh
@@ -10,12 +10,11 @@ esac
 echo "Using CRaC enabled JDK"
 
 cd ../../../
-#mvn -f pom-quarkus.xml clean package
 docker build -t randomstrings-crac:builder -f src/main/docker/quarkus/Dockerfile.crac-jvm --build-arg CRAC_JDK_URL=$url .
 echo "==> Done building, attempting to RUN"
 docker run -d --privileged --rm -i --name=randomstrings-crac --ulimit nofile=1024 -p 8080:8080 -v $(pwd)/../../../target:/opt/mnt -e FLAG=$1 randomstrings-crac:builder
 echo "==> RUN successful"
-#siege -c 1 -r 100000 -b http://localhost:8080/api/rnd
+#siege -c 1 -r 150000 -b http://localhost:8080/api/rnd
 echo "Please wait during checkpoint creation..."
 sleep 10
 docker commit --change='ENTRYPOINT ["/opt/app/entrypoint.sh"]' $(docker ps -qf "name=randomstrings-crac") randomstrings-crac:checkpoint
