89fa9722b0800883ebbf6743bfdf806ee2cb4f18
Rustam Mehmandarov <mehmandarov@gmail.com>
2025-03-03 16:30:59 -0500

    Cleanup checkpoint.sh

--- src/scripts/crac/checkpoint.sh
+++ src/scripts/crac/checkpoint.sh
@@ -4,31 +4,25 @@ set -e
 
 echo "=> Using CRaC enabled JDK."
 
-echo "==> Starting application build"
-cd ../../../
+echo "==> Starting application container build"
 docker build -t randomstrings-quarkus-crac:builder -f src/main/docker/quarkus/Dockerfile-CRaC.jvm .
-echo "===> Done building application"
+echo "> Done building application"
 
-echo "==> Attempting to start the container and warm-up the app."
-# Check if the container exists and remove it if it does
-if docker ps -a --format '{{.Names}}' | grep -q "^randomstrings-quarkus-crac$"; then
-    echo "===> Removing existing container: randomstrings-quarkus-crac"
-    docker rm -f randomstrings-quarkus-crac
-fi
-docker run -d --privileged --rm -i --name=randomstrings-quarkus-crac --ulimit nofile=1024 -p 8080:8080 -v "$(pwd)"/../../../target:/opt/mnt -e FLAG=$1 randomstrings-quarkus-crac:builder
-echo "===> Application startup was successful"
-
-echo "==> JVM and application warmup"
-# Explanation of the one-liner below:
-# * seq 1 20000: Generates numbers from 1 to 20,000.
-# * xargs -I{} -P 10: Runs 10 parallel requests at a time (-P 10).
-# * wget -qO- http://localhost:8080/api/rnd > /dev/null: Fetches the endpoint without outputting anything.
-# seq 1 20000 | xargs -I{} -P 10 curl -s http://localhost:8080/api/rnd > /dev/null
-seq 1 10 | xargs -I{} -P 10 curl -s http://localhost:8080/api/rnd
-echo "===> Done running JVM and application warmup"
+echo "==> Attempting to start the application container."
+docker run -d --privileged --rm -i --name=randomstrings-quarkus-crac --ulimit nofile=1024 -p 8080:8080 -v "$(pwd)"/crac-files:/opt/crac-files randomstrings-quarkus-crac:builder
+echo "> Application startup was successful"
 
 echo "==> Creating checkpoint. Please wait..."
 sleep 20
-docker commit --change='ENTRYPOINT ["/opt/app/entrypoint.sh"]' "$(docker ps -qf 'name=randomstrings-quarkus-crac')" randomstrings-quarkus-crac:checkpoint
-docker kill "$(docker ps -qf 'name=randomstrings-quarkus-crac')"
-echo "===> Done creating a container image with checkpoint."
+
+# Check if the container exists before taking the snapshot
+CONTAINER_ID=$(docker ps -qf 'name=randomstrings-quarkus-crac')
+
+if [ -n "$CONTAINER_ID" ]; then
+    echo "> Container found. Taking snapshot..."
+    docker commit --change='ENTRYPOINT ["/opt/app/entrypoint.sh"]' "$CONTAINER_ID" randomstrings-quarkus-crac:checkpoint
+    docker kill "$CONTAINER_ID"
+    echo "> Done creating a container image with checkpoint."
+else
+    echo "> No running container found. Skipping snapshot. Remove contents of crac-files/ folder to take a new snapshot."
+fi
