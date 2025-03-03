89f66c5325e54d428bbf2da4674f5db51707a3dc
Rustam Mehmandarov <mehmandarov@gmail.com>
2025-03-03 12:50:00 -0500

    Cleanup in entrypoint script and fix PID conflicts during checkpoint restores.

--- src/scripts/crac/entrypoint.sh
+++ src/scripts/crac/entrypoint.sh
@@ -3,20 +3,18 @@
 CRAC_FILES_DIR=`eval echo ${CRAC_FILES_DIR}`
 mkdir -p $CRAC_FILES_DIR
 
+# This is a workaround for resolving PID conflicts.
+# More info: https://docs.azul.com/core/crac/crac-debugging.html#resolving-pid-conflicts
+echo 128 > /proc/sys/kernel/ns_last_pid;
+
 if [ -z "$(ls -A $CRAC_FILES_DIR)" ]; then
-  echo "==> CHECKPOINT: No checkpoint found. Creating one."
-  if [ "$FLAG" = "-r" ]; then
-    java -XX:CRaCCheckpointTo=$CRAC_FILES_DIR -jar /opt/app/quarkus-run.jar
-  else
-    java -XX:CRaCCheckpointTo=$CRAC_FILES_DIR -jar /opt/app/quarkus-run.jar&
-    sleep 5
-    jcmd /opt/app/quarkus-run.jar JDK.checkpoint
-  fi
+  echo "==> Hello from the container >> CHECKPOINT: No checkpoint found. Creating one."
+  java -XX:CRaCCheckpointTo=$CRAC_FILES_DIR -jar /opt/app/quarkus-run.jar&
+  sleep 10
+  jcmd /opt/app/quarkus-run.jar JDK.checkpoint
   sleep infinity
 else
-  echo "==> CHECKPOINT: Restoring from checkpoint."
-  echo "==> CHECKPOINT: DEBUG: \n $(ls -a $CRAC_FILES_DIR)"
-  #java -XX:CRaCRestoreFrom=$CRAC_FILES_DIR&
+  echo "==> Hello from the container >> CHECKPOINT: Restoring from checkpoint."
   java -XX:CRaCRestoreFrom=$CRAC_FILES_DIR -XX:+UnlockExperimentalVMOptions -XX:+IgnoreCPUFeatures&
   PID=$!
   trap "kill $PID" SIGINT SIGTERM
