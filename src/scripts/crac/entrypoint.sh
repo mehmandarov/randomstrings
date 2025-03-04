1ee0a5ac5c36135af1ab3b5205990fbd8f4b4bc6
Rustam Mehmandarov <mehmandarov@gmail.com>
2025-03-04 17:41:12 -0500

    Switch CRaC engine to Warp. Fix restore.sh script.

--- src/scripts/crac/entrypoint.sh
+++ src/scripts/crac/entrypoint.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-CRAC_FILES_DIR=`eval echo ${CRAC_FILES_DIR}`
-mkdir -p $CRAC_FILES_DIR
+CRAC_FILES_DIR=$(eval echo "${CRAC_FILES_DIR}")
+mkdir -p "$CRAC_FILES_DIR"
 
 # This is a workaround for resolving PID conflicts.
 # More info: https://docs.azul.com/core/crac/crac-debugging.html#resolving-pid-conflicts
 echo 128 > /proc/sys/kernel/ns_last_pid;
 
-if [ -z "$(ls -A $CRAC_FILES_DIR)" ]; then
-  echo "==> Hello from the container >> CHECKPOINT: No checkpoint found. Creating one."
-  java -XX:CRaCCheckpointTo=$CRAC_FILES_DIR -jar /opt/app/quarkus-run.jar&
+if [ -z "$(ls -A "$CRAC_FILES_DIR")" ]; then
+  echo "=> Hello from the container >> CHECKPOINT: No checkpoint found. Creating one."
+  java -XX:CRaCEngine=warp -XX:CRaCCheckpointTo="$CRAC_FILES_DIR" -jar /opt/app/quarkus-run.jar&
   sleep 10
   jcmd /opt/app/quarkus-run.jar JDK.checkpoint
   sleep infinity
 else
-  echo "==> Hello from the container >> CHECKPOINT: Restoring from checkpoint."
-  java -XX:CRaCRestoreFrom=$CRAC_FILES_DIR -XX:+UnlockExperimentalVMOptions -XX:+IgnoreCPUFeatures&
+  echo "=> Hello from the container >> CHECKPOINT: Restoring from checkpoint."
+  java -XX:CRaCEngine=warp -XX:CRaCRestoreFrom="$CRAC_FILES_DIR" -XX:+UnlockExperimentalVMOptions -XX:+IgnoreCPUFeatures&
   PID=$!
-  trap "kill $PID" SIGINT SIGTERM
+  trap 'kill "$PID"' SIGINT SIGTERM
   wait $PID
 fi
--- src/scripts/crac/restore.sh
+++ src/scripts/crac/restore.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-echo "New phone. Who dis?"
+echo "=> Restoring from Checkpoint: New container. Who dis?"
 
 # SETPCAP => Modify process capabilities.
 # https://docs.docker.com/engine/reference/run/#runtime-privilege-and-linux-capabilities
-docker run --privileged --cap-add CHECKPOINT_RESTORE --cap-add SETPCAP --cap-add SYS_RESOURCE --rm -p 8080:8080 --name randomstrings-quarkus-crac randomstrings-quarkus-crac:checkpoint
+docker run --privileged --cap-add CHECKPOINT_RESTORE --cap-add SETPCAP --cap-add SYS_RESOURCE --rm -p 8080:8080 -v "$(pwd)"/crac-files:/opt/crac-files --name randomstrings-quarkus-crac randomstrings-quarkus-crac:checkpoint
